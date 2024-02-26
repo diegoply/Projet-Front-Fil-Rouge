e7bd19fc61df7cc2d0d70ce1b2ad81251b865c41
Diego Ply <ply.dgo@gmail.com>
2024-02-26 18:37:59 +0100

    ajout button icons

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changement de mots de passe", "/pages/auth/editPassword.html"),
 ];
 
     export const websiteName = "Quai Antique";
