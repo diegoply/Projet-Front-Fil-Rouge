ec7ddcfd7b7f9427422af7775a7337e0aedfe094
Diego Ply <ply.dgo@gmail.com>
2024-02-26 11:14:39 +0100

    debut galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,6 +2,8 @@ import Route from "./Route.js";
 
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
 ];
 
     export const websiteName = "Quai Antique";
+
