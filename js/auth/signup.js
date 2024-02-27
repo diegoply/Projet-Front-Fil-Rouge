189668fefb97f641f560ca075832fd8b0a4a2a95
Diego Ply <ply.dgo@gmail.com>
2024-02-27 18:03:50 +0100

    confirmation info formulaire

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -5,6 +5,7 @@ const inputPreNom = document.getElementById("PrenomInput");
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidationPassword = document.getElementById("ValidatePasswordInput");
+const btnValidation = document.getElementById("btn-validation-inscription");
 
 inputNom.addEventListener("keyup", validateForm);
 inputPreNom.addEventListener("keyup", validateForm);
@@ -13,17 +14,61 @@ inputPassword.addEventListener("keyup", validateForm);
 inputValidationPassword.addEventListener("keyup", validateForm);
 
 function validateForm(){
-    validateRequired(inputNom);
-    validateRequired(inputPreNom);
+
+
+    const nomOk = validateRequired(inputNom);
+    const prenomOk = validateRequired(inputPreNom);
+    const mailOk = validateMail(inputMail);
+    const passwordOk = validatePassword(inputPassword);
+
+    if(nomOk && prenomOk && mailOk && passwordOk){
+        btnValidation.disabled = false;
+    }
+    else{
+        btnValidation.disabled = true;
+    }
 }
+
+function validateMail(input){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+    if(mailUser.match(emailRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
+    }
+
+    function validatePassword(input){
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+        const passwordUser = input.value;
+        if(passwordUser.match(passwordRegex)){
+            input.classList.add("is-valid");
+            input.classList.remove("is-invalid");
+            return true;
+        }
+        else{
+            input.classList.remove("is-valid");
+            input.classList.add("is-invalid");
+            return false;
+        }
+        }
+
 
 function validateRequired(input){
     if(input.value != ''){
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
+        return true;
     }
     else{
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
+        return false;
     }
 }
