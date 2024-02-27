17f36a098cba26ee1b77c22f1786b02260e1d26e
Diego Ply <ply.dgo@gmail.com>
2024-02-27 10:10:49 +0100

    debut dynamiser la partie front

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,6 +7,8 @@ export const allRoutes = [
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mots de passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 
     export const websiteName = "Quai Antique";
